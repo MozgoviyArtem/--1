ccdb8be4d5881e4230e462efd9cf72cd909c2beb
MozgoviyArtem <inmax0106@ukr.net>
2025-09-19 19:56:35 +0300

    Додавання функції

---
+++ script.js
@@ -0,0 +1,27 @@
+const moment = require('moment')
+
+function getCurrentDay(){
+    console.log(moment().format('dddd'))
+}
+function getCurrentMonth(){
+    console.log(moment().format('mmm'))
+}
+function getCurrentYear(){
+    console.log(moment().format('yyy'))
+}
+function getDate () {
+    const now = moment();
+    console.log(now.format("YYYY/MM/DD HH:mm:ss"));
+}
+function getCurrentWeekday() {
+    const days = [
+        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+    ];
+    const today = new Date();
+    console.log("Today is" , days[today.getDate()])
+}
+getCurrentWeekday
+getDate();
+getCurrentDay();
+getCurrentMonth();
+getCurrentYear();
