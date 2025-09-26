9710e762e132c878a32f1e7fc74eddd2b70845ce
MozgoviyArtem <inmax0106@ukr.net>
2025-09-26 23:14:41 +0300

    API and query параметри

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const moment = require('moment');
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { error } = require('console');
 const app = express();
 const HOST = 'localhost';
 const PORT = 8018;
 
+const postsFilePath = path.join(__dirname, 'posts.json');
+
+app.get('/posts', (req, res) => {
+  fs.readFile(postsFilePath, 'utf8', (err, data) => {
+    if (err) {
+      return res;
+    }
+    const posts = JSON.parse(data);
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+      return res.status(400).json({ error: 'ID має бути числом' });
+    }
+
+    const post = posts.find(p => p.id === id);
+
+    if (!post) {
+      return res.status(404).json({ error: 'Пост з ID не знайдено' });
+    }
+
+    res.json(post);
+  });
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`http://${HOST}:${PORT}`);
+});
 function getCurrentTimestamp(){
     return new Date(). toISOString();
 }
