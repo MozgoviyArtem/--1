c19b5824988dd8596eacda0d8f0c75b4ef244dd3
MozgoviyArtem <inmax0106@ukr.net>
2025-10-02 20:53:54 +0300

    try catch, promises, validation

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const moment = require('moment');
 const express = require('express');
+const fspromises = require('fs/promises');
 const fs = require('fs');
 const path = require('path');
 const { error } = require('console');
-const app = express();
+const app = express();////////
+app.use(express.json())//////////
 const HOST = 'localhost';
 const PORT = 8018;
 
@@ -48,8 +50,6 @@ app.get('/posts', (req, res) => {
     }
 
     res.json(post);
-  });
-});
 
 app.listen(PORT, HOST, () => {
   console.log(`http://${HOST}:${PORT}`);
@@ -62,10 +62,45 @@ app.get('/timestamp', (req,res) =>{
     res.json({ timestamp : getCurrentTimestamp()})
 })
 
+const FILE_PATH = "./posts.json";
+
+app.post("/posts", async (req, res) => {
+  try {
+    const { title, description, image } = req.body;
+
+    if (!title  || !description || !image) {
+      return res.status(422).json({ error: "Потрібні поля: title, description, image" });
+    }
+
+    let posts = [];
+    try {
+      const data = await fs.readFile(FILE_PATH, "utf-8");
+      posts = JSON.parse(data);
+    } catch {
+      posts = [];
+    }
+
+    const newPost = {
+      id: posts.length + 1,
+      title,
+      description,
+      image,
+    };
+
+    posts.push(newPost);
+    await fs.writeFile(FILE_PATH, JSON.stringify(posts, null, 2));
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Внутрішня помилка сервера" });
+  }
+});
+
 app.listen(PORT,HOST, ()=>{
     console.log("Сервер запущено на http://localhost:8018")
 })
-
+///////////////////////////////////////////////
 function getCurrentDay(){
     console.log(moment().format('dddd'))
 }
